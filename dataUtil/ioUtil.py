d669363a19820ac60d0035b9fbe01b631a4ea4a2
Arvind Yalavarti <yalavartiarvind@gmail.com>
2019-11-20 22:30:33 -0500

    Split and merge audio files

---
+++ dataUtil/ioUtil.py
@@ -0,0 +1,38 @@
+from matplotlib import pyplot as plt
+import numpy as np
+from constants import *
+from scipy.io import wavfile
+
+
+def export_audio_data(filepath, data):
+    np.save(filepath, data)
+
+
+def read_audio_data(filepath):
+    return np.load(filepath)
+
+
+def plot_audio_segment(sample_rate, data, overlayed=False, ranges=None,
+                       filename="plot.png"):
+    sample_rate = float(sample_rate)
+    times = np.arange(len(data)) / sample_rate
+    plt.figure(figsize=(300, 10))
+    plt.xlim(times[0], times[-1])
+    plt.fill_between(times, data[:, 0], data[:, 1], color='k')
+    if overlayed:
+        for start, end in ranges:
+            if end // MS < times[-1]:
+                plt.axvspan(start / MS, end / MS, alpha=0.2, color='red')
+            else:
+                break
+
+    plt.xlabel('Time (s)')
+    plt.ylabel('Amplitude s(t)')
+    plt.savefig(filename, dpi=100)
+    plt.show()
+
+
+def export_segmented_audio_wav(audio_clips, filename, sample_rate):
+    for i, segment in enumerate(audio_clips):
+        wavfile.write("./data/{0}-clip{1}.wav".format(filename, i),
+                      sample_rate, segment)
--- dataUtil/processing.py
+++ dataUtil/processing.py
@@ -1,33 +1,14 @@
-from scipy.io import wavfile
-from matplotlib import pyplot as plt
 import numpy as np
+import math
+import scipy.io as sio
 from pydub import AudioSegment
 from pydub.silence import detect_nonsilent
-
-MS = 1000
-
-
-def plot_audio_segment(sample_rate, data, overlayed=False, ranges=None,
-                       filename="plot.png"):
-    sample_rate = float(sample_rate)
-    times = np.arange(len(data)) / sample_rate
-    # TODO: make figsize a function of data dimensions
-    plt.figure(figsize=(100, 10))
-    plt.xlim(times[0], times[-1])
-    plt.fill_between(times, data[:, 0], data[:, 1], color='k')
-    # TODO: check range based on data
-    if overlayed:
-        for start, end in ranges:
-            plt.axvspan(start / MS, end / MS, alpha=0.2, color='red')
-
-    plt.xlabel('Time (s)')
-    plt.ylabel('Amplitude s(t)')
-    plt.savefig(filename, dpi=100)
-    plt.show()
+from ioUtil import *
+from constants import *
 
 
 def get_non_silent_ranges(filepath, audio_length, silence_length=1000,
-                          silence_thresh=-50):
+                          silence_thresh=-52):
     audio_file = AudioSegment.from_wav(filepath)
     audio_file = audio_file[:audio_length * MS]
     ranges = detect_nonsilent(audio_file, min_silence_len=silence_length,
@@ -35,17 +16,48 @@ def get_non_silent_ranges(filepath, audio_length, silence_length=1000,
     return ranges
 
 
-def process_audio_file(filepath):
+def pad_audio_clip(clip, sample_rate):
+    seg_target_len = SEG_LEN * sample_rate
+    pad_len = int(seg_target_len - len(clip))
+    return np.pad(clip, ((0, pad_len), (0, 0)), mode='constant')
+
+
+def split_audio_clip(clip, sample_rate):
+    split = []
+    n = math.ceil(len(clip) / (sample_rate * SEG_LEN))
+    for s in np.array_split(clip, n):
+        split.append(pad_audio_clip(s, sample_rate))
+    return split
+
+
+def merge_audio_clips(data, ranges, sample_rate):
+    segmented = []
+    for start, end in ranges:
+        f_start, f_end = int((start / MS) * sample_rate),\
+                         int((end / MS) * sample_rate)
+        seg = data[f_start:f_end]
+        if len(seg) / sample_rate > SEG_LEN:
+            segmented.extend(split_audio_clip(seg, sample_rate))
+        else:
+            segmented.append(pad_audio_clip(seg, sample_rate))
+    return np.asarray(segmented)
+
+
+def process_audio_file(filepath, savefile):
     if filepath.lower().endswith(".wav"):
+        filename = filepath.split(".wav")[0].split("/")[-1]
         # Sampling rate, given in Hz, is number of measurements per second
-        sample_rate, data = wavfile.read(filepath)
-        non_silent_ranges = get_non_silent_ranges(filepath, 100)
-        # Multiply second * sample rate gives number of frames in raw WAV data
-        plot_audio_segment(sample_rate, data[:100 * sample_rate],
-                           overlayed=True,
-                           ranges=non_silent_ranges,
-                           filename=filepath.split(".wav")[0] + "-segmented"
-                                                                "-plot")
+        sample_rate, data = sio.wavfile.read(filepath)
+        non_silent_ranges = get_non_silent_ranges(filepath, 240)
+        # Multiply second * sample rate to get number of frames in raw WAV data
+        # plot_audio_segment(sample_rate, data[:130 * sample_rate],
+        #                    overlayed=True,
+        #                    ranges=non_silent_ranges,
+        #                    filename=filepath.split(".wav")[0] + "-segmented"
+        #                                                         "-plot2")
+        merged = merge_audio_clips(data, non_silent_ranges, sample_rate)
+        export_segmented_audio_wav(merged[:3], filename, sample_rate)
+        export_audio_data(savefile, merged)
 
     else:
         print("File format not recognized.")
@@ -53,4 +65,4 @@ def process_audio_file(filepath):
 
 
 if __name__ == '__main__':
-    process_audio_file("./data/raw/english1.wav")
+    process_audio_file("./data/raw/english1.wav", "./data/processed/test1.npy")
