92517a74232d321efdc491003598f207dadb845c
Arvind Yalavarti <yalavartiarvind@gmail.com>
2019-11-26 12:14:35 -0500

    Refactor featureExtraction.py

--- dataUtil/__init__.py
+++
@@ -1,2 +0,0 @@
-from processing import *
-from constants import *
--- dataUtil/featureExtraction.py
+++ dataUtil/featureExtraction.py
@@ -1,33 +1,41 @@
-import numpy as np
-from scipy.signal import spectrogram as spectro
-from dataUtil.processing import read_audio_data
-from dataUtil.constants import NUM_FREQUENCIES, NUM_TESTING_CLIPS
-from librosa.feature import mfcc
-from librosa.display import specshow
 import matplotlib.pyplot as plt
+import numpy as np
+import ioUtil as io
+
+from librosa.display import specshow
+from librosa.feature import mfcc
+from scipy.signal import spectrogram as spectro
+
+from constants import *
 
 
 def get_fft(signal, sampling_frequency, testing=False):
     """
-    This function takes in an array, either of size (num_frames,) or (num_batches, num_frames)
-        and returns a tuple of times, frequencies, and a spectrogram.
+    This function takes in an array, either of size (num_frames,) or
+    (num_batches, num_frames) and returns a tuple of times, frequencies, and a
+    spectrogram.
+
     :param signal: the signal to get the spectrogram from
     :param sampling_frequency: the sampling frequency of the signal
     :param testing: whether or not to run as testing
     :return: a tuple of segment times (shape: [num_batches,] num_segments),
-                        sample frequncies (shape: [num_baches,] num_frequencies),
-                        spectrogram (shape: [num_batches,] num_segments, num_frequencies)
+             sample frequencies (shape: [num_batches,] num_frequencies),
+             spectrogram (shape: [num_batches,] num_segments, num_frequencies)
     """
     if testing:
         signal = signal[:NUM_TESTING_CLIPS]
-    frequencies, times, spectrogram = spectro(signal, fs=sampling_frequency, nfft=2048)
+    frequencies, times, spectrogram = spectro(signal, fs=sampling_frequency,
+                                              nfft=2048)
     # Only return important frequencies
-    return times, frequencies[:NUM_FREQUENCIES], np.swapaxes(spectrogram, 1, 2)[:, :, :NUM_FREQUENCIES]
+    return times, frequencies[:NUM_FREQUENCIES], \
+           np.swapaxes(spectrogram, 1, 2)[:, :, :NUM_FREQUENCIES]
 
 
 def _get_single_mfcc(signal, sampling_frequency, num_mfcc):
     """
-    This function takes in an array of size (num_frames,) and returns the mfccs of size (num_segments, num_mfcc)
+    This function takes in an array of size (num_frames,) and returns the mfccs
+    of size (num_segments, num_mfcc)
+
     :param signal: the signal to get the mfccs from
     :param sampling_frequency: the sampling frequency of the signal
     :param num_mfcc: the number of mfccs to extract
@@ -39,7 +47,8 @@ def _get_single_mfcc(signal, sampling_frequency, num_mfcc):
 def get_mfcc(signals, sampling_frequency, num_fcc, testing=False):
     """
     This function takes in a batch of signals of size (num_batches, num_frames,)
-        and returns the mfccs of size (num_batches, num_segments, num_mfcc)
+    and returns the mfccs of size (num_batches, num_segments, num_mfcc)
+
     :param signals: the signal to get the mfccs from
     :param sampling_frequency: the sampling frequency of the signal
     :param num_fcc: the number of mfccs to extract
@@ -82,19 +91,25 @@ def plot_mfcc(mfcc_data):
 
 
 def main():
-    a = read_audio_data("./data/processed/english/english.npy").astype(np.float32)
-    b = read_audio_data("./data/processed/chinese/chinese.npy").astype(np.float32)
-    c = read_audio_data("./data/processed/british/british.npy").astype(np.float32)
+    a = io.read_audio_data("./data/processed/english/english.npy").astype(
+        np.float32)
+    b = io.read_audio_data("./data/processed/chinese/chinese.npy").astype(
+        np.float32)
+    c = io.read_audio_data("./data/processed/british/british.npy").astype(
+        np.float32)
 
-    english_chinese_sr = 22050
-    british_sr = 44100
-    segment_times1, sample_frequencies1, spectrogram1 = get_fft(a, english_chinese_sr, testing=True)
-    segment_times2, sample_frequencies2, spectrogram2 = get_fft(b, english_chinese_sr, testing=True)
-    segment_times3, sample_frequencies3, spectrogram3 = get_fft(c, british_sr, testing=True)
+    segment_times1, sample_frequencies1, spectrogram1 = get_fft(a,
+                                                                SAMPLE_RATE,
+                                                                testing=False)
+    segment_times2, sample_frequencies2, spectrogram2 = get_fft(b,
+                                                                SAMPLE_RATE,
+                                                                testing=False)
+    segment_times3, sample_frequencies3, spectrogram3 = get_fft(c, SAMPLE_RATE,
+                                                                testing=False)
 
-    mfcc1 = get_mfcc(a, english_chinese_sr, 12, testing=True)
-    mfcc2 = get_mfcc(b, english_chinese_sr, 12, testing=True)
-    mfcc3 = get_mfcc(c, british_sr, 12, testing=True)
+    mfcc1 = get_mfcc(a, SAMPLE_RATE, 12, testing=False)
+    mfcc2 = get_mfcc(b, SAMPLE_RATE, 12, testing=False)
+    mfcc3 = get_mfcc(c, SAMPLE_RATE, 12, testing=False)
 
     print(a.shape)
     print(b.shape)
