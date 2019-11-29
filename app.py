69738fc4b1fc825def0e0ce8faf37699afe2b0d4
Arvind Yalavarti <yalavartiarvind@gmail.com>
2019-11-29 16:34:41 -0500

    Finish /classify route

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template, request
+from matplotlib import pyplot as plt
 import subprocess
+import scipy.io.wavfile
+import numpy as np
+from dataUtil.featureExtraction import segment_and_extract
 import os
 
 template_dir = os.path.abspath('./web/templates')
@@ -12,3 +16,18 @@ def init_accenter():
     return render_template('Accenter.html')
 
 
+@app.route("/classify", methods=['POST'])
+def classify_accent():
+    rawAudio = request.get_data()
+
+    audioFile = open('recordedAudio.wav', 'wb')
+    audioFile.write(rawAudio)
+    audioFile.close()
+
+    spectrogram, mfccs = segment_and_extract('recordedAudio.wav')
+    if spectrogram is None:
+        return "ERROR"
+    else:
+        accent = np.random.choice(["british", "english", "chinese", "korean"], 1)[0]
+        print(accent)
+        return accent.capitalize()
