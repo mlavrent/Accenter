2ad970a18ce6146a7231f448e19cb87ef5a5ad8d
Mark Lavrentyev <lavrema@outlook.com>
2019-11-28 23:54:37 -0500

    Modify get class method and add accuracy method

--- models/classification/cnn.py
+++ models/classification/cnn.py
@@ -1,13 +1,14 @@
 import tensorflow as tf
 import tensorflow.keras as k
+import numpy as np
 
 
 class ClassifyCNN(k.Model):
     def __init__(self, accent_classes):
         super(ClassifyCNN, self).__init__()
 
-        self.accent_classes = accent_classes
-        self.num_accent_classes = len(self.accent_classes)
+        self.accent_classes = np.array(accent_classes)
+        self.num_accent_classes = len(accent_classes)
         self.type = "classifier"
 
         self.optimizer = k.optimizers.Adam(3e-3)
@@ -61,14 +62,25 @@ class ClassifyCNN(k.Model):
 
         return cur_calc
 
-    def get_class(self, inp):
+    def get_class(self, inputs):
         """
-        Given an input, gets the class that is most likely (highest prob). Useful for evaluation.
-        :param inp: A single input example of size (..., ..., 1?)
+        Given inputs, gets the class that is most likely (highest prob) for each.
+        Useful for evaluation.
+        :param inputs: A list of inputs to predict class for
         :return: The accent class that is most likely for the input
         """
-        class_probs = self.call(tf.constant([inp]))
-        return self.accent_classes[tf.argmax(class_probs)]
+        class_probs = self.call(inputs)
+        return self.accent_classes[tf.argmax(class_probs, axis=1)]
+
+    def accuracy(self, inputs, labels):
+        """
+        Given inputs and sparse labels (indices), calculates the accuracy of predictions.
+        :param inputs: Batch of model inputs.
+        :param labels: Batch of sparse label indices (1d tensor)
+        :return: 0-1 accuracy value
+        """
+        predictions = tf.argmax(self.call(inputs), axis=1)
+        return tf.reduce_mean(predictions == labels)
 
     def loss(self, inputs, labels):
         """
