0c3715de65d3985cccece80783f17c5a4b5fdfdf
Jeffrey Zhu <jzhu@jeffreys-mbp.devices.brown.edu>
2019-12-07 19:07:35 -0500

    add lstm model

--- models/classification/lstm.py
+++ models/classification/lstm.py
@@ -13,26 +13,32 @@ class ClassifyLSTM(k.Model):
 
         self.optimizer = k.optimizers.Adam(3e-3)
         self.batch_size = 100
+        self.embedding_size = 20
 
         # TODO: set up layers here
+        self.rnn = tf.keras.layers.GRU(self.embedding_size, return_sequences=True, return_state=True)
+        self.dense1 = tf.keras.layers.Dense(self.embedding_size, activation='relu')
+        self.dense2 = tf.keras.layers.Dense(self.num_accent_classes, activation='softmax')
 
     @tf.function
     def call(self, inputs):
         """
-        Run a forward pass of the CNN classification model.
+        Run a forward pass of the LSTM classification model.
         :param inputs: Tensor or np array of size (batchSize, ..., ..., 1?)
         :return: Tensor of size (batchSize, numAccents)
         """
-        ...
+        rnn_output, state = self.rnn(inputs)
+        return self.dense2(self.dense1(state))
 
-    def get_class(self, inp):
+    def get_class(self, inputs):
         """
-        Given an input, gets the class that is most likely (highest prob). Useful for evaluation.
-        :param inp: A single input example of size (..., ..., 1?)
+        Given inputs, gets the class that is most likely (highest prob) for each.
+        Useful for evaluation.
+        :param inputs: A list of inputs to predict class for
         :return: The accent class that is most likely for the input
         """
-        class_probs = self.call(tf.constant([inp]))
-        return self.accent_classes[tf.argmax(class_probs)]
+        class_probs = self.call(inputs)
+        return self.accent_classes[tf.argmax(class_probs, axis=1)]
 
     def accuracy(self, inputs, labels):
         """
@@ -44,10 +50,12 @@ class ClassifyLSTM(k.Model):
         predictions = tf.argmax(self.call(inputs), axis=1)
         return tf.reduce_mean(predictions == labels)
 
-    def loss(self):
+    def loss(self, inputs, labels):
         """
         Calculates the loss for some batch of input-label pairs.
         :param inputs: Tensor of size (batchSize, ..., ..., 1?)
+        :param labels: Tensor of size (batchSize, 1) - sparse labels indicating index of accent
         :return: The average loss over this batch.
         """
-        ...
+        logits = self.call(inputs)
+        return tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels, logits))
